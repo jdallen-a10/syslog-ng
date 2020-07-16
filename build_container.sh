5ad1492542981560e7a9b12143997cd954905b27
jdallen-a10 <64568514+jdallen-a10@users.noreply.github.com>
2020-07-16 14:09:14 -0500

    Update build_container.sh

--- build_container.sh
+++ build_container.sh
@@ -1,2 +1,1 @@
-VERSION=$(cat VERSION | perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e' | tee VERSION)
-docker build -t jdallen/syslog-ng:$VERSION -t jdallen/syslog-ng:latest https://github.com/jdallen-a10/syslog-ng.git
+docker build -t a10networks/syslog-ng:latest https://github.com/jdallen-a10/syslog-ng.git
